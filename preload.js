35bb0a7cbdcc9a93ccf47e8125b10c9537f33c7f
jrzdagoat <jrzaltygm@gmail.com>
2026-09-12 00:02:21 +0100

    Add generateKey method to contextBridge

--- preload.js
+++ preload.js
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("jrz", {
   checkKey: (key) => ipcRenderer.invoke("auth:check", key),
+  generateKey: (owner) => ipcRenderer.invoke("auth:generateKey", owner),
   authSuccess: () => ipcRenderer.invoke("auth:success"),
   getSlots: () => ipcRenderer.invoke("slots:get"),
   pickFiles: (multi) => ipcRenderer.invoke("files:pick", { multi }),
